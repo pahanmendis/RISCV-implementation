ea1a808e5c76f257eb8a1366ade130bbca44ec1c
pahanmendis <53089732+pahanmendis@users.noreply.github.com>
2023-02-01 04:10:29 +0530

    Update control_unit.v
    updated internal logic blocks - have to complete the logic

--- RISCV.srcs/sources_1/new/control_unit.v
+++ RISCV.srcs/sources_1/new/control_unit.v
@@ -21,16 +21,45 @@
 
 
 module control_unit(
-    input clk,
-    input [7:0] opcode,
-    output branch,
-    output mem_read,
-    output mem_to_reg,
-    output ALU_op,
-    output mem_write,
-    output ALU_Src,
+    input ID_clk,
+    input [6:0] func7,
+    input [2:0] func3,
+    output [2:0] im_slice,
+    output [1:0] extend,
+    output alu_source,
+    output [1:0] branch,
+    output [2:0] to_reg,
+    output [1:0] mem_read,
+    output [1:0] mem_write,
     output reg_write
     );
     
     //write control unit
+    parameter  control_signal_length=15; //36-1=35
+    reg [control_signal_length:0] control_store[1023:0];
+    reg [15:0] control_out;
+        
+    
+    initial 
+    begin
+    
+        control_store[0]  = 16'b0011000001100001;  //LUI
+        control_store[1]  = 16'b0011000001100001;  //insert control signals
+    
+    end
+    
+    always @(*)
+        begin
+        control_out<=control_store[{func3,func7}];
+        end
+
+    
+    assign im_slice = control_out[15:13];
+    assign extend = control_out[12:11];
+    assign alu_source = control_out[10];
+    assign branch = control_out[9:8];
+    assign to_reg = control_out[7:5];
+    assign mem_read = control_out[4:3];
+    assign mem_write = control_out[2:1];
+    assign reg_write = control_out[0];
 endmodule
