3aab194ac5dbaf831b6a2d44cbebe03a70a51a90
pahanmendis <53089732+pahanmendis@users.noreply.github.com>
2023-01-31 20:53:08 +0530

    Update data_mem.v
    updated ports and bus widths

--- RISCV.srcs/sources_1/new/data_mem.v
+++ RISCV.srcs/sources_1/new/data_mem.v
@@ -21,31 +21,33 @@
 
 
 module data_mem(
-    input clk,
-    input mem_write,
-    input [63:0] data_address,
-    input [63:0] write_data,
-    output reg [63:0] read_data
+    input MEM_clk,
+    input [1:0] mem_write,
+    input [1:0] mem_read,
+    input [31:0] data_address,
+    input [31:0] write_data,
+    output reg [31:0] read_data
     );
     parameter mem_depth = 199; //depth-1
-    reg [63:0] data_ram [mem_depth:0];    
+    reg [31:0] data_ram [mem_depth:0];    
     
     initial   //if required to store initial values
         begin
-            data_ram[0] = {64'b0}; //AC <- 0 
-            data_ram[1] = {64'b0}; //MAR <- AC 
+            data_ram[0] = {31'b0}; //AC <- 0 
+            data_ram[1] = {31'b0}; //MAR <- AC 
         end    
     
-    always@(posedge clk)                    //output data
+    always@(posedge MEM_clk)                    //output data
         begin
         read_data<=data_ram[data_address];
         end
 
-    always@(posedge clk)                    //output data
+    always@(posedge MEM_clk)                    //output data
         begin
-        if (mem_write==1'b1)      //check logic to write to data mem
+        if (mem_write==2'b11)      //check logic to write to data mem
             begin
                 data_ram[data_address]<=write_data;
+                read_data<=31'd0;
             end
         end
     
